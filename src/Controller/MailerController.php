96f0b9a0aceb02587bf8ec15c213f9d90db5f4d1
Jokaas <jokubka@gmail.com>
2021-05-09 14:37:31 +0300

    Added Simfony Google mailer. Updated Simfony mailer Can send test email.

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email")
+     */
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from(new Address('jokaastest@gmail.com', 'Watson'))
+            ->to('jokubka@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            ->replyTo('noreply@jokaastest.lt')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        //$mailer->send($email);
+
+
+    }
+}
